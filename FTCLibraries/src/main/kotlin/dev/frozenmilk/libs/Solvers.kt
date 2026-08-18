c64fc25df9dc0fec1d18f5865afa76d939fb4c1e
Arush <arush.yadlapati@gmail.com>
2026-08-18 18:45:02 +0530

    Update Solvers to 0.3.5
    Added Photon too

--- FTCLibraries/src/main/kotlin/dev/frozenmilk/libs/Solvers.kt
+++ FTCLibraries/src/main/kotlin/dev/frozenmilk/libs/Solvers.kt
@@ -14,6 +14,7 @@ class Solvers(ftc: FTC) : EasyAutoScope<Solvers>(ftc) {
         )
     }
 
-    val core by dependency("0.3.3")
-    val pedroPathing by dependency("0.3.3")
+    val core by dependency("0.3.5")
+    val pedroPathing by dependency("0.3.5")
+    val photon by dependency("0.3.5")
 }
